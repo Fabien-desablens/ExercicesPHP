b090896a4ef2f53d1f08e7ece52b2c783b1bf3ba
Fabien <49678708+Fabien-desablens@users.noreply.github.com>
2019-07-17 10:13:35 +0200

    Add files via upload

---
+++ DrillPHP/1_Variables/Exercices_5/index.php
@@ -0,0 +1,5 @@
+<?php
+$res1 = 3+4;
+$res2 = 5*20;
+$res3 = 45/5;
+var_dump($res1,$res2,$res3);
---
+++ DrillPHP/1_Variables/Exercices_6/index.php
@@ -0,0 +1,5 @@
+<?php
+$prixdépart = 785;
+$ristourne = $prixdépart*(30/100);
+$prix = $prixdépart-$ristourne;
+var_dump($prixdépart,$ristourne,$prix);
